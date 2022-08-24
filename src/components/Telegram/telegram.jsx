531db6fc503d0eeff3ec2b29accbb57a286cd9e0
Elena Kazakova <61557802+N0vum@users.noreply.github.com>
2022-08-24 17:11:10 +0700

    Update telegram.jsx

--- src/components/Telegram/telegram.jsx
+++ src/components/Telegram/telegram.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import { StyledTelegram } from "./telegram.styled";
 
-const Telegram = () => {
+const Telegram = ({ active }) => {
   return (
-    <StyledTelegram>
-      <p className="text_telegram-invite">
-        Зайди в мой Telegram-бот и забери методичку по закрытию возражения
-        ДОРОГО
-      </p>
+    <StyledTelegram active={active}>
+      <div className="text_telegram-invite">
+        <div className="text_telegram-invite_paragraph">
+          <p>📨</p>
+          <p>Заявка на бесплатную консультацию получена!</p>
+        </div>
+        <div className="text_telegram-invite_paragraph">
+          <p>📅</p>
+          <p>Я свяжусь с тобой для согласования удобного времени.</p>
+        </div>
+        <div className="text_telegram-invite_paragraph">
+          <p>🎁</p>
+          <p>
+            Получи методичку по закрытию возражения "ДОРОГО" в моем Telegram
+          </p>
+        </div>
+      </div>
       <button
         className="btn_telegram"
-        onClick={() => window.open("https://tglink.ru/inna_bron_bot", "_blank")}
+        onClick={() => {
+          window.open("https://tglink.ru/inna_bron_bot", "_blank");
+          active
+            ? window.ym(90054720, "reachGoal", "button1-submint")
+            : window.ym(90054720, "reachGoal", "button2-submint");
+        }}
       >
         <img src="../photos/Icons/telegram_white.png" alt="telegram" />
       </button>
